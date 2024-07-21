e694ab72d19686eddb823fcd27954972d3788f15
Ashish050488 <ashishar050488@gmail.com>
2024-07-22 00:21:03 +0530

    data coming from swiggy live restaurants api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,23 @@
-import resList from "../utils/MockData"
 import RestaurentCard from "./RestaurentCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = ()=>{
 
-    const [ListOfRestaurnt, setListOfRestaurent] = useState(resList)
+    const [ListOfRestaurnt, setListOfRestaurent] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+        // console.log(setListOfRestaurent);
+    },[]);
+    
+    const fetchData = async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        // console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants[0]);
+    
+        // optional chaining operator (?) is used to avoid errors if the property is not found it will return undefined instead of an error. and in UI we can use optional chaining operator to avoid errors.
+        setListOfRestaurent(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
 
     return(
         <div className="body">
@@ -18,6 +31,7 @@ const Body = ()=>{
                 >Top rated restaurant</button>
         </div>
         <div className="res-container">
+            {console.log(ListOfRestaurnt)}
             {ListOfRestaurnt.map((restaurant) => (
                 <RestaurentCard key={restaurant.id} resData={restaurant} />
             ))}
--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -3,17 +3,17 @@ import { CDN_URL } from "../utils/constant";
 
 
 const RestaurentCard = (props)=>{
-    const{resData} = props; 
-    const{cloudinaryImageId,cuisines,name,costForTwo,avgRatingString,sla}=resData
+    const { resData } = props;
+    const { name, cuisines, costForTwo, avgRatingString, sla, cloudinaryImageId } = resData.info;
 
     return(
         <div className="res-card">
             <img className="res-logo"  alt="res-logo" src={CDN_URL+ cloudinaryImageId}/>
             <h3 className="name">{name}</h3>
-            <h3>{cuisines.join(", ")}</h3>
+            <h3>{cuisines?.join(", ")}</h3>
             <h3>{costForTwo}</h3>
             <h3>Rating{avgRatingString}⭐</h3>
-            <h3>{sla.slaString}</h3>
+            <h3>{sla?.slaString}</h3>
         </div>
     )
 };
