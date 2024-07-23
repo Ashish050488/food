90d6c2450a082c5b7dd45188265887d46cf78af7
Ashish050488 <ashishar050488@gmail.com>
2024-07-23 22:27:09 +0530

    search field is created and router is configured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import Header from "./components/Header.js"
 import Body from "./components/Body.js";
+import About from "./components/About.js";
+import Error from "./components/Error.js";
+import Contact from "./components/Contact.js";
+
 
 
 
@@ -14,7 +19,27 @@ const Applayout = ()=>{
         <Header/>
         <Body/>
     </div>
-}
+};
+
+const AppRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<Applayout/>,
+        errorElement:<Error/>
+    },
+    {
+        path:"/about",
+        element:<About/>,
+    },
+    {
+       path:"/contact",
+       element:<Contact/> 
+    },
+    {
+        path:"/error",
+        element:<Error/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applayout/>);
+root.render(<RouterProvider router={AppRouter}/>);
--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,15 @@ import { Swiggy_API } from "../utils/constant";
 const Body = ()=>{
 
     const [ListOfRestaurnt, setListOfRestaurent] = useState([]);
+    const [filteredRestaurants,setfilteresRestaurants] = useState([])
 
+    const [searchText,setsearchText] = useState("")
+
+
+
+    // whenever state variables update, react triggers a reconciliation cycle(re-render the  component);
+
+    // console.log("body rendered");
     useEffect(()=>{
         fetchData();
         // console.log(setListOfRestaurent);
@@ -19,6 +27,7 @@ const Body = ()=>{
     
         // optional chaining operator (?) is used to avoid errors if the property is not found it will return undefined instead of an error. and in UI we can use optional chaining operator to avoid errors.
         setListOfRestaurent(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setfilteresRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
 
 
@@ -27,18 +36,31 @@ const Body = ()=>{
     return ListOfRestaurnt.length===0 ? (<CircularColor/>): (
         <div className="body">
         <div className="filter">
+            <div className="search">
+                <input type="text" className="search-box" value={searchText} onChange={(e)=> setsearchText(e.target.value)} />
+                <button onClick={()=>{
+                    //filter the restaurent card and update the ui
+                    const filteredRestaurent = ListOfRestaurnt.filter(
+                        (res) => res.info.name.toLowerCase().includes(searchText.toLocaleLowerCase())
+                    );
+
+                    // There is a bug that if data is searchText is not found it goes blank and just a loader is loaded
+                    setfilteresRestaurants(filteredRestaurent);
+                    // console.log(filteredRestaurent);
+                    
+                }} >Search</button>
+            </div>
             <button className="filter-btn" onClick={
                         () => {
-                        const FilteredList = ListOfRestaurnt.filter((res) => Number(res.avgRatingString) > 4);
-                        setListOfRestaurent(FilteredList); // Update state with filtered list
+                        const FilteredList = ListOfRestaurnt.filter((res) => Number(res.info.avgRatingString) > 4);
+                        setfilteresRestaurants(FilteredList); // Update state with filtered list
                         }
                     }
                 >Top rated restaurant</button>
         </div>
         <div className="res-container">
-            {console.log(ListOfRestaurnt)}
-            {ListOfRestaurnt.map((restaurant) => (
-                <RestaurentCard key={restaurant.id} resData={restaurant} />
+            {filteredRestaurants.map((restaurant) => (
+                <RestaurentCard key={restaurant.info.id} resData={restaurant} />
             ))}
         </div>
         
